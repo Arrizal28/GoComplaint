86f6455e91141a6ff58606b61cbca1d1c201f419
Arrizal28 <2010631170088@student.unsika.ac.id>
2023-12-02 20:44:08 +0700

    feat/auth_page

---
+++ app/src/main/java/com/bangkit/gocomplaint/ui/components/BasicButton.kt
@@ -0,0 +1,42 @@
+package com.bangkit.gocomplaint.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bangkit.gocomplaint.R
+import com.bangkit.gocomplaint.ui.theme.poppinsFontFamily
+
+@Composable
+fun BasicButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = {onClick() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+        ),
+        shape = RoundedCornerShape(size = 10.dp),
+        modifier = modifier
+    ) {
+        Text(text = text,
+            fontFamily = poppinsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 16.sp,
+            color = Color.White)
+    }
+}
--- app/src/main/java/com/bangkit/gocomplaint/ui/theme/Color.kt
+++ app/src/main/java/com/bangkit/gocomplaint/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val OliveGreen = Color(0xFF618264)
+val AsparagusGreen = Color(0xFF79AC78)
+val PaleRobinEgggreen = Color(0xFFB0D9B1)
+val Grey = Color(0xFF505050)
